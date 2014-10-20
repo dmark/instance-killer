9ac7a0d8aed186d41794137af0171e1696ad1fd0
root <root@server.com>
2014-10-20 16:19:23 +0000

    Testing beta

--- killer.sh
+++ killer.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
 
-list=`aws ec2 describe-instances | jsawk 'return this.Reservations' | jsawk 'return this.Instances[0]' | jsawk -n 'out(this.InstanceId + "+" + this.LaunchTime + "+" + this.SubnetId + "+" + this.PrivateIpAddress)'`
+list=`aws ec2 describe-instances --region eu-west-1 | jsawk 'return this.Reservations' | jsawk 'return this.Instances[0]' | jsawk -n 'out(this.InstanceId + "+" + this.LaunchTime + "+" + this.SubnetId + "+" + this.PrivateIpAddress)'`
 
 for i in $list
 do
 	IFS='+' read -a array <<< "$i"
-        limit=`date -d "3 days ago" +%s`
-        launch=`date --utc --date "${array[1]/T/\ }" +%s`
-	echo $now
-	echo $launch
-	echo ${array[0]};
-	echo ${array[1]/T/\ };
-	echo ${array[2]};
-	echo ${array[3]};
-        n=`aws ec2 describe-instances --instance-ids ${array[0]} | jsawk 'return this.Reservations[0].Instances[0]' | jsawk 'return this.Tags[0].Value'`
-	echo $n
+        killlimit=`date -d "$1 hours ago" +%s`
+        warnlimit=`date -d "$2 hours ago" +%s`
+	launch=${array[1]/T/\ };
+        launch=`date --utc --date "$launch" +%s`
+	declare -A warn
+	declare -A kill
+
+       if [ "$launch" -lt "$warnlimit" ] && [ "$launch" -gt "$killlimit" ]
+           then
+
+                n=`aws ec2 describe-instances --region eu-west-1 --instance-ids ${array[0]} | jsawk 'return this.Reservations[0].Instances[0]' | jsawk 'return this.Tags[0].Value'`
+		warn["${array[0]}"]='{"InstanceId":"'"${array[0]}"'","LaunchTime":"'"${array[1]}"'","SubnetId":"'"${array[2]}"'","PrivateIpAddress":"'"${array[3]}"'","Name":"'"$n"'"}'
+
+        fi
+
+        if [ "$launch" -lt "$killlimit" ]
+	   then
+
+        	n=`aws ec2 describe-instances --region eu-west-1 --instance-ids ${array[0]} | jsawk 'return this.Reservations[0].Instances[0]' | jsawk 'return this.Tags[0].Value'`
+		kill["${array[0]}"]='{"InstanceId":"'"${array[0]}"'","LaunchTime":"'"${array[1]}"'","SubnetId":"'"${array[2]}"'","PrivateIpAddress":"'"${array[3]}"'","Name":"'"$n"'"}'
+	fi
+	
 done
+
+echo '####################'
+echo "Instances on warning"
+echo '####################'
+for x in "${warn[@]}"
+do
+   :
+   echo '~~~~~~~~~~~~~~~';
+   echo $x | jsawk -n 'out(this.InstanceId+"\n"+this.LaunchTime+"\n"+this.SubnetId+"\n"+this.PrivateIpAddress+"\n"+this.Name)'
+   echo '~~~~~~~~~~~~~~~';
+done
+
+echo '####################'
+echo "Instances killed"
+echo '####################'
+for x in "${kill[@]}"
+do
+   :
+   echo '~~~~~~~~~~~~~~~';
+   echo $x | jsawk -n 'out(this.InstanceId+"\n"+this.LaunchTime+"\n"+this.SubnetId+"\n"+this.PrivateIpAddress+"\n"+this.Name)'
+   echo '~~~~~~~~~~~~~~~';
+done
+
+
