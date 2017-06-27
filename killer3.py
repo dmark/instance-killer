a6696560c2793f3fb96769fcbd601f3044edd49e
Mark Drummond <mark@markdrummond.ca>
2017-06-27 09:06:32 -0400

    minor housekeeping

--- killer3.py
+++ killer3.py
@@ -1,20 +1,22 @@
-# killer3.py - boto3-based version of the killer.py script.
+#!/usr/bin/env python2
+# killer3.py - terminate long running EC2 instances
 
-# Add the ability to specify which AWS CLI profile you want to use. You must
-# specify the '-y' option to actually terminate instances.
+"""killer3.py
 
-import argparse
+Terminates long running EC2 instances. The number of hours an instance must be
+up before being terminated can be defined on the command line. The user must
+explicitly pass the '-y' or '--yes' option before instances will be terminated.
+Otherwise the script does not make any changes.
+"""
 
-from boto3 import session, ec2
 from datetime import tzinfo, timedelta, datetime
-from pprint import PrettyPrinter
 
 ZERO = timedelta(0)
 HOUR = timedelta(hours=1)
 
 # A UTC class.
 class UTC(tzinfo):
-    """UTC"""
+    """Creates a UTC instance of the tzinfo class"""
 
     def utcoffset(self, dt):
         return ZERO
@@ -25,38 +27,62 @@ class UTC(tzinfo):
     def dst(self, dt):
         return ZERO
 
-utc = UTC()
 
-pp = PrettyPrinter(indent=4)
+def get_args():
+    """Processes command line arguments"""
+    import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--terminate", type=int, help="Terminate instances older than this many hours", default=72)
-parser.add_argument("-w", "--warn", type=int, help="Warn about any instances older than this many hours", default=36)
-parser.add_argument("-p", "--profile", help="The awscli profile name you wish to use", default="default")
-parser.add_argument("-r", "--region", help="The AWS region you want to interact with", default="us-east-1")
-parser.add_argument("-y", "--yes", help="Terminate all instances older than the specified termination time", action="store_true")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--terminate", type=int,
+                        help="Terminate instances older than this many hours (default 72)",
+                        default=72)
+    parser.add_argument("-w", "--warn", type=int,
+                        help="Warn for any instances older than this many hours (default 36)",
+                        default=36)
+    parser.add_argument("-p", "--profile",
+                        help="The awscli profile name you wish to use (default 'default')",
+                        default="default")
+    parser.add_argument("-r", "--region",
+                        help="The AWS region you want to interact with (default us-east-1)",
+                        default="us-east-1")
+    parser.add_argument("-y", "--yes",
+                        help="Terminate instances (default is 'do nothing')",
+                        action="store_true")
+    return(parser.parse_args())
 
-now = datetime.now(utc)
-delta_kill = now - timedelta(hours=args.terminate)
-delta_warn = now - timedelta(hours=args.warn)
 
-session = session.Session(region_name=args.region, profile_name=args.profile)
-ec2 = session.client('ec2')
-response = ec2.describe_instances()
+def main():
 
-warn_instances = []
-for reservation in response[u'Reservations']:
-    for instance in reservation[u'Instances']:
-        launchtime = instance[u'LaunchTime']
-        if launchtime < delta_kill and args.yes:
-            print "Terminating instance", instance[u'InstanceId']
-            #ec2.terminate_instances(InstanceIds=[instance[u'InstanceId']])
-        elif launchtime < delta_kill and not args.yes:
-            print "Skipping instance", instance[u'InstanceId']
-        elif launchtime < delta_warn:
-            warn_instances += instance
+    from boto3 import session, ec2
+    from pprint import PrettyPrinter
 
-if warn_instances:
-    print "The following instances are more than ", args.warn, "hrs old."
-    pp.pprint(warn_instances)
+    utc = UTC()
+    pp = PrettyPrinter(indent=4)
+    args = get_args()
+
+    delta_kill = datetime.now(utc) - timedelta(hours=args.terminate)
+    delta_warn = datetime.now(utc) - timedelta(hours=args.warn)
+
+    session = session.Session(region_name=args.region, profile_name=args.profile)
+    ec2 = session.client('ec2')
+    response = ec2.describe_instances()
+
+    warn_instances = []
+    for reservation in response[u'Reservations']:
+        for instance in reservation[u'Instances']:
+            launchtime = instance[u'LaunchTime']
+            if launchtime < delta_kill and args.yes:
+                print "Terminating instance", instance[u'InstanceId']
+                #ec2.terminate_instances(InstanceIds=[instance[u'InstanceId']])
+            elif launchtime < delta_kill and not args.yes:
+                print "Skipping instance", instance[u'InstanceId']
+            elif launchtime < delta_warn:
+                warn_instances += instance
+
+    if warn_instances:
+        print "The following instances are more than ", args.warn, "hrs old."
+        pp.pprint(warn_instances)
+
+
+if __name__ == '__main__':
+    main()
